caba8f9480da23d7ca82e8684a046c109a8a2849
fr-kevin <kevin.oneil@fastretailing.com>
2018-11-19 10:40:15 +0900

    offline mode works

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const react = require("react");
+const ReactDom = require("react-dom");
+const OfflinePlugin = require('offline-plugin/runtime');
+
+OfflinePlugin.install();
+
+console.log(OfflinePlugin);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const webpack = require("webpack");
+const OfflinePlugin = require("offline-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/template/index.html")
+    }),
+    new OfflinePlugin({
+      caches: {
+        main: [
+          'index.html',
+          'index.js',
+          'dist/index.html',
+          'dist/index.js',
+        ],
+    })
+  ]
+};
